e7a6f80499acb58788ac0ea60558c4470104d3bc
mwaterous <mark.waterous>
2014-06-30 21:58:26 +0000

    Cleaning these up for the next release

--- includes/class.processor.php
+++ includes/class.processor.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Submit a listing view controller and other functionality
  *
@@ -8,170 +9,174 @@
  * @link      http://lddwebdesign.com
  * @copyright 2014 LDD Consulting, Inc
  */
-
-
 class ldd_directory_lite_processor {
 
-	const NONCE_FIELD  = 'nonce_field';
-	const NONCE_ACTION = 'submit-listing-nonce';
+    const NONCE_FIELD = 'nonce_field';
+    const NONCE_ACTION = 'submit-listing-nonce';
 
-	const DATA_PREFIX = 'ld_s_';
+    const DATA_PREFIX = 'ld_s_';
 
-	protected $processing = false;
-	protected $extra_fields;
-	protected $data;
-	protected $errors;
-	protected $global_errors;
+    protected $processing = false;
+    protected $extra_fields;
+    protected $data;
+    protected $errors;
+    protected $global_errors;
 
-
-	public function __construct() {
-
-		if ( !array_key_exists( self::NONCE_FIELD, $_POST ) )
-			return;
-
-		$this->processing = true;
-
-		$this->_verify_nonce();
-		$this->_process_data();
-		$this->_validate();
-
-	}
+    protected $required_fields = array(
+        'title',
+        'category',
+        'description',
+        'summary'
+    );
 
 
-	private function _verify_nonce() {
-		if ( !wp_verify_nonce( $_POST[ self::NONCE_FIELD ], self::NONCE_ACTION ) )
-			die( "No, kitty! That's a bad kitty!" );
-	}
+    public function __construct() {
+
+        if (!array_key_exists(self::NONCE_FIELD, $_POST))
+            return;
+
+        $this->processing = true;
+
+        $this->_verify_nonce();
+        $this->_process_data();
+        $this->_validate();
+
+    }
 
 
-	private function _process_data() {
-
-		$pfx_length = strlen( self::DATA_PREFIX );
-
-		foreach ( $_POST as $key => $value ) {
-
-			if ( false === strpos( $key, self::DATA_PREFIX ) ) {
-				$this->extra_fields[ $key ] = $value;
-				continue;
-			}
-
-			$data_key = substr( $key, $pfx_length );
-
-			if ( is_array( $value ) ) {
-				$this->data[ $data_key ] = stripslashes_deep( $value );
-			} else {
-				$this->data[ $data_key ] = stripslashes( trim( $value ) );
-			}
-
-		}
-
-	}
+    private function _verify_nonce() {
+        if (!wp_verify_nonce($_POST[ self::NONCE_FIELD ], self::NONCE_ACTION))
+            die("No, kitty! That's a bad kitty!");
+    }
 
 
-	private function _validate() {
+    private function _process_data() {
 
-		$required = ldl_get_required_fields();
+        $pfx_length = strlen(self::DATA_PREFIX);
 
-		foreach ( $required as $key ) {
-			if ( '' == $this->data[ $key ] ) {
-				$this->errors[ $key ] = __( 'This field is required.', 'lddlite' );
-			}
-		}
+        foreach ($_POST as $key => $value) {
 
-		foreach( $this->data as $key => $value ) {
-			// Use this if you want to add a single function to the validation process
-			$error = apply_filters( 'lddlite_validate_fields', '', $key, $value );
-			// Or this if you want to alter the validation of a single field
-			$error = apply_filters( 'lddlite_validate_field_' . $key, $error, $value );
+            if (false === strpos($key, self::DATA_PREFIX)) {
+                $this->extra_fields[ $key ] = $value;
+                continue;
+            }
 
-			if ( !empty( $error ) ) {
-				$this->errors[ $key ] = $error;
-			}
+            $data_key = substr($key, $pfx_length);
 
-		}
+            if (is_array($value)) {
+                $this->data[ $data_key ] = stripslashes_deep($value);
+            } else {
+                $this->data[ $data_key ] = stripslashes(trim($value));
+            }
 
-	}
+        }
 
-
-	public function is_processing() {
-		return $this->processing;
-	}
+    }
 
 
-	public function get_data() {
-		return $this->data;
-	}
+    private function _validate() {
+
+        $required = apply_filters('lddlite_submit_required_fields', $this->required_fields);
 
 
-	public function get_value( $key ) {
-		return isset( $this->data[ $key ] ) ? $this->data[ $key ] : '';
-	}
+        foreach ($required as $key) {
+            if ('' == $this->data[ $key ]) {
+                $this->errors[ $key ] = __('This field is required.', 'lddlite');
+            }
+        }
+
+        foreach ($this->data as $key => $value) {
+            // Use this if you want to add a single function to the validation process
+            $error = apply_filters('lddlite_validate_fields', '', $key, $value);
+            // Or this if you want to alter the validation of a single field
+            $error = apply_filters('lddlite_validate_field_' . $key, $error, $value);
+
+            if (!empty($error)) {
+                $this->errors[ $key ] = $error;
+            }
+
+        }
+
+    }
 
 
-	public function has_error( $key ) {
-		return isset( $this->errors[ $key ] );
-	}
+    public function is_processing() {
+        return $this->processing;
+    }
 
 
-	public function has_errors() {
-		return ( !empty( $this->errors ) || !empty( $this->global_errors ) );
-	}
+    public function get_data() {
+        return $this->data;
+    }
 
 
-	public function has_global_errors() {
-		return ( !empty( $this->global_errors ) );
-	}
+    public function get_value($key) {
+        return isset($this->data[ $key ]) ? $this->data[ $key ] : '';
+    }
 
 
-	public function get_error( $key ) {
-		if ( isset( $this->errors[ $key ] ) ) {
-			echo '<span class="text-danger">' . $this->errors[ $key ] . '</span>';
-		}
-	}
+    public function has_error($key) {
+        return isset($this->errors[ $key ]);
+    }
 
 
-	public function set_global_error( $errormsg ) {
-		$this->global_errors[] = $errormsg;
-	}
+    public function has_errors() {
+        return (!empty($this->errors) || !empty($this->global_errors));
+    }
 
 
-	public function get_global_errors() {
-		return each( $this->global_errors );
-	}
+    public function has_global_errors() {
+        return (!empty($this->global_errors));
+    }
+
+
+    public function get_error($key) {
+        if (isset($this->errors[ $key ])) {
+            echo '<span class="text-danger">' . $this->errors[ $key ] . '</span>';
+        }
+    }
+
+
+    public function set_global_error($errormsg) {
+        $this->global_errors[ ] = $errormsg;
+    }
+
+
+    public function get_global_errors() {
+        return each($this->global_errors);
+    }
 }
 
 
-function ldl_validate_fields( $error, $field, $value ) {
+function ldl_validate_fields($error, $field, $value) {
 
-	if ( empty( $value ) )
-		return $error;
+    if (empty($value))
+        return $error;
 
-	switch ( $field ) {
-		case 'contact_email':
-			if ( !is_email( $value ) ) {
-				$error = __( "That email address doesn't appear to be valid.", 'lddlite' );
-			}
-			break;
-		case 'url_website':
-		case 'url_facebook':
-		case 'url_linkedin':
-			if ( 0 !== strpos( $value, 'http' ) ) {
-				$value = esc_url( $value );
-			}
-			if ( false != filter_var( $value, FILTER_VALIDATE_URL ) ) {
-				$error = __( 'We were unable to verify that URL, please check it and try again.', 'lddlite' );
-			}
-			break;
-		case 'geo':
-			if ( !is_array( $value )
-				|| 3 != count( $value )
-				|| in_array( '', $value )
-				|| !preg_match( '/^(\-?\d+(\.\d+)?),\s*(\-?\d+(\.\d+)?)$/', $value['lat'] . ',' . $value['lng'] ) ) {
-				$error = __( 'Something went wrong validating that location, please try again.', 'lddlite' );
-			}
-			break;
-	}
+    switch ($field) {
+        case 'contact_email':
+            if (!is_email($value)) {
+                $error = __("That email address doesn't appear to be valid.", 'lddlite');
+            }
+            break;
+        case 'url_website':
+        case 'url_facebook':
+        case 'url_linkedin':
+            if (0 !== strpos($value, 'http')) {
+                $value = esc_url($value);
+            }
+            if (false != filter_var($value, FILTER_VALIDATE_URL)) {
+                $error = __('We were unable to verify that URL, please check it and try again.', 'lddlite');
+            }
+            break;
+        case 'geo':
+            if (!is_array($value) || 3 != count($value) || in_array('', $value) || !preg_match('/^(\-?\d+(\.\d+)?),\s*(\-?\d+(\.\d+)?)$/', $value[ 'lat' ] . ',' . $value[ 'lng' ])) {
+                $error = __('Something went wrong validating that location, please try again.', 'lddlite');
+            }
+            break;
+    }
 
-	return $error;
+    return $error;
 }
-add_filter( 'lddlite_validate_fields', 'ldl_validate_fields', 10, 3 );
+
+add_filter('lddlite_validate_fields', 'ldl_validate_fields', 10, 3);
--- includes/shortcodes/submit.php
+++ includes/shortcodes/submit.php
@@ -10,227 +10,286 @@
  */
 
 
+/** The following are template functions relevant only to this shortcode **/
+
 /**
- * This is a hot mess and is just going to wind up even more fragmented at this rate.
- * Start brainstorming how to rebuild this before it's too late.
- * BEGIN PROCESS.PHP
- **********************************************************************************************************************/
+ * Alias for wp_dropdown_categories() that uses our settings array to determine the output.
+ */
+function ldl_submit_categories_dropdown($selected = 0, $id = 'category', $classes = array('form-control')) {
+
+    if (is_string($classes))
+        $classes = explode(' ', $classes);
+
+    $classes = apply_filters('lddlite_categories_dropdown_class', $classes);
+
+    $pfx = ldd_directory_lite_processor::DATA_PREFIX;
+    $name = $pfx . $id;
+
+    $category_args = array(
+        'hide_empty'   => 0,
+        'echo'         => 0,
+        'selected'     => $selected,
+        'hierarchical' => 1,
+        'name'         => $name,
+        'id'           => $id,
+        'class'        => implode(' ', $classes),
+        'tab_index'    => 2,
+        'taxonomy'     => LDDLITE_TAX_CAT,
+    );
+
+    echo wp_dropdown_categories($category_args);
+}
 
 
-function ldl_submit__create_user( $username, $email ) {
+function ldl_get_value($key) {
+    global $lddlite_submit_processor;
 
-    $password = wp_generate_password( 14, true );
-    $user_id = wp_create_user( $username, $password, $email );
+    return $lddlite_submit_processor->get_value($key);
+}
 
-    if ( $user_id )
-        wp_new_user_notification( $user_id, $password );
+function ldl_get_error($key) {
+    global $lddlite_submit_processor;
+
+    return $lddlite_submit_processor->get_error($key);
+}
+
+
+function ldl_has_errors() {
+    global $lddlite_submit_processor;
+
+    return $lddlite_submit_processor->has_errors();
+}
+
+
+function ldl_has_global_errors() {
+    global $lddlite_submit_processor;
+
+    return $lddlite_submit_processor->has_global_errors();
+}
+
+
+function ldl_get_global_errors() {
+    global $lddlite_submit_processor;
+
+    return $lddlite_submit_processor->get_global_errors();
+}
+
+/** End of template functions **/
+
+
+/**
+ * Create a new user during the submission process. If successful, send them an internal email providing them
+ * with their login information.
+ *
+ * @param string $username The username (this should have been checked during processing)
+ * @param string $email    Same as above, this should already be valid
+ *
+ * @return int|object Returns the user_id if successful, WP_Error if not.
+ */
+function ldl_submit_create_user($username, $email) {
+
+    $password = wp_generate_password(14, true);
+    $user_id = wp_create_user($username, $password, $email);
+
+    if (!is_wp_error($user_id)) {
+        wp_new_user_notification($user_id, $password);
+    }
 
     return $user_id;
 }
 
 
-function ldl_submit__create_listing( $name, $description, $cat_ID, $user_id ) {
+/**
+ * Create the post associated with the new listing. This takes information from the submit form, and a $user_id
+ * that was generated previously in ldl_submit_create_user()
+ *
+ * @param string $name The listing/post title
+ * @param string $description The description, which may contain markdown
+ * @param int $cat_id The taxonomy ID for this listing
+ * @param int $user_id The author of this listing
+ *
+ * @return int|WP_Error A valid $post_id on success, and the WP_Error object on failure
+ */
+function ldl_submit_create_post($name, $description, $cat_id, $user_id) {
 
-    $listing = array(
-        'post_content'  => $description,
-        'post_title'    => $name,
-        'post_status'   => 'pending',
-        'post_type'     => LDDLITE_POST_TYPE,
-        'post_author'   => $user_id,
-        'post_date'     => date( 'Y-m-d H:i:s' ),
+    $args = array(
+        'post_content' => $description,
+        'post_title'   => $name,
+        'post_status'  => 'pending',
+        'post_type'    => LDDLITE_POST_TYPE,
+        'post_author'  => $user_id,
+        'post_date'    => date('Y-m-d H:i:s'),
     );
 
-    $post_ID = wp_insert_post( $listing );
+    $post_id = wp_insert_post($args);
 
-    if ( $post_ID ) {
-        wp_set_object_terms( $post_ID, (int) $cat_ID, LDDLITE_TAX_CAT );
-        return $post_ID;
+    if (!is_wp_error($post_id)) {
+        wp_set_object_terms($post_id, (int) $cat_id, LDDLITE_TAX_CAT);
     }
 
-    return false;
+    return $post_id;
 }
 
 
-function ldl_submit__create_meta( $data, $post_id ) {
+function ldl_submit_create_meta($data, $post_id) {
 
-    $remove_fields = array(
-        'title',
-	    'category',
-        'description',
-        'summary',
-        'username',
-	    'email',
-    );
+    $remove_fields = array('title', 'category', 'description', 'summary', 'username', 'email',);
 
-    $data = array_diff_key( $data, array_flip( $remove_fields ) );
+    $data = array_diff_key($data, array_flip($remove_fields));
 
-    foreach ( $data as $key => $value ) {
-        add_post_meta( $post_id, LDDLITE_PFX . $key, $value );
+    foreach ($data as $key => $value) {
+        add_post_meta($post_id, LDDLITE_PFX . $key, $value);
     }
 
 }
 
 
-function ldl_submit__email_admin( array $data, $post_id ) {
+function ldl_submit_notify_admin($data, $post_id) {
 
-    $subject = ldl_get_setting( 'email_toadmin_subject' );
-    $message = ldl_get_setting( 'email_toadmin_body' );
+    $to = ldl_get_setting('email_notifications');
+    $subject = ldl_get_setting('email_toadmin_subject');
+    $message = ldl_get_setting('email_toadmin_body');
 
-    $message = str_replace( '{aprove_link}', admin_url( 'post.php?post=' . $post_id . '&action=edit' ), $message );
-    $message = str_replace( '{title}', $data['title'], $message );
-    $message = str_replace( '{description}', $data['description'], $message );
+    $message = str_replace('{aprove_link}', admin_url('post.php?post=' . $post_id . '&action=edit'), $message);
+    $message = str_replace('{title}', $data[ 'title' ], $message);
+    $message = str_replace('{description}', $data[ 'description' ], $message);
 
-    ldl_mail( ldl_get_setting( 'email_notifications' ), $subject, $message );
+    ldl_mail($to, $subject, $message);
 }
 
 
-function ldl_submit__email_owner( array $data ) {
+function ldl_submit_notify_author($data) {
 
-    $subject = ldl_get_setting( 'email_onsubmit_subject' );
-    $message = ldl_get_setting( 'email_onsubmit_body' );
+    $to = $data[ 'email' ];
+    $subject = ldl_get_setting('email_onsubmit_subject');
+    $message = ldl_get_setting('email_onsubmit_body');
 
-    $message = str_replace( '{site_title}', get_bloginfo( 'name' ), $message );
-    $message = str_replace( '{directory_title}', ldl_get_setting( 'directory_label' ), $message );
-    $message = str_replace( '{directory_email}', ldl_get_setting( 'email_from_address' ), $message );
-    $message = str_replace( '{title}', $data['title'], $message );
-    $message = str_replace( '{description}', $data['description'], $message );
+    $message = str_replace('{site_title}', get_bloginfo('name'), $message);
+    $message = str_replace('{directory_title}', ldl_get_setting('directory_label'), $message);
+    $message = str_replace('{directory_email}', ldl_get_setting('email_from_address'), $message);
+    $message = str_replace('{title}', $data[ 'title' ], $message);
+    $message = str_replace('{description}', $data[ 'description' ], $message);
 
-    ldl_mail( $data['email'], $subject, $message );
+    ldl_mail($to, $subject, $message);
 }
 
 
 /**
- * Alias for wp_dropdown_categories() that uses our settings array to determine the output.
+ * Used by ldl_submit_generate_listing() to destroy any evidence that we started to generate a listing. This is a
+ * stopgap measure to allow the form to fail and be resubmitted. I think this can be better, but I want to get to
+ * stable before I spend more time on it.
+ *
+ * @since 0.5.5
+ *
+ * @param array $ids An associate array identifying what needs to be deleted
  */
-function ldl_submit_categories_dropdown( $selected = 0, $id = 'category', $classes = array( 'form-control' ) ) {
-	global $data;
-
-	if ( is_string( $classes ) )
-		$classes = explode( ' ', $classes );
-
-	$classes = apply_filters( 'lddlite_categories_dropdown_class', $classes );
-
-	$pfx = ldd_directory_lite_processor::DATA_PREFIX;
-	$name = $pfx . $id;
-
-	$category_args = array(
-		'hide_empty'    => 0,
-		'echo'          => 0,
-		'selected'      => $selected,
-		'hierarchical'  => 1,
-		'name'          => $name,
-		'id'            => $id,
-		'class'         => implode( ' ', $classes ),
-		'tab_index'     => 2,
-		'taxonomy'      => LDDLITE_TAX_CAT,
-	);
-
-	echo wp_dropdown_categories( $category_args );
+function ldl_submit_rollback($ids) {
+    foreach ($ids as $id) {
+        switch ($id) {
+            case 'user_id':
+                wp_delete_user($id);
+                break;
+            case 'post_id':
+                wp_delete_post($id);
+                break;
+        }
+    }
 }
 
 
-function ldl_get_required_fields() {
+/**
+ * This is responsible for generating the listing based on the information provided in the form submission. Everything
+ * should be sanitized and validated by now via the $lddlite_submit_processor object, however since errors are still
+ * possible, I want this to be able to rollback on items that are created, or store their IDs for use on the next pass.
+ *
+ * @since 0.5.5
+ * @return bool True only if successful, false if any errors occur
+ */
+function ldl_submit_generate_listing() {
+    global $lddlite_submit_processor;
 
-	$map = array(
-		'title',
-		'category',
-		'description',
-		'summary',
-	);
+    $data = $lddlite_submit_processor->get_data();
 
-	return apply_filters( 'lddlite_submit_required_fields', $map );
+    // If anything fails, we need to start over.
+    // I considered storing the IDs as they were generated, and simply setting back on whatever failed,
+    // but that won't work unless I have a way of disabling form fields on the fly.
+
+    $user_id = is_user_logged_in() ? get_current_user_id() : ldl_submit_create_user($data[ 'username' ], $data[ 'email' ]);
+    if (is_wp_error($user_id)) {
+        $lddlite_submit_processor->set_global_error(__('There was a problem creating your user account. Please try again later.', 'lddlite'));
+
+        return false;
+    }
+
+    $post_id = ldl_submit_create_post($data[ 'title' ], $data[ 'description' ], $data[ 'category' ], $user_id);
+    if (!$post_id) {
+        $lddlite_submit_processor->set_global_error(__('There was a problem creating your listing. Please try again later.', 'lddlite'));
+        ldl_submit_rollback(array(
+            'user_id' = $user_id,
+        ));
+
+        return false;
+    }
+
+    // Add all the post meta fields
+    ldl_submit_create_meta($data, $post_id);
+
+    // Upload their logo if one was submitted
+    if (isset($_FILES[ 'ld_s_logo' ])) {
+        // These files need to be included as dependencies when on the front end.
+        require_once(ABSPATH . 'wp-admin/includes/image.php');
+        require_once(ABSPATH . 'wp-admin/includes/file.php');
+        require_once(ABSPATH . 'wp-admin/includes/media.php');
+
+        $attachment_id = media_handle_upload('ld_s_logo', 0);
+        if (is_wp_error($attachment_id)) {
+            $lddlite_submit_processor->set_global_error(__('There was a problem uploading your logo. Please try again!', 'lddlite'));
+            ldl_submit_rollback(array(
+                'user_id' = $user_id,
+                'post_id' = $post_id,
+            ));
+
+            return false;
+        } else {
+            set_post_thumbnail($post_id, $attachment_id);
+        }
+    }
+
+    ldl_submit_notify_admin($data, $post_id); // Notification of new listing
+    ldl_submit_notify_author($data); // Receipt of submission
+
+    return true;
 }
 
 
-function ldl_get_value( $key ) {
-	global $lddlite_submit_processor;
-
-	return $lddlite_submit_processor->get_value( $key );
-}
-
-function ldl_get_error( $key ) {
-	global $lddlite_submit_processor;
-
-	return $lddlite_submit_processor->get_error( $key );
-}
-
-
-function ldl_has_errors() {
-	global $lddlite_submit_processor;
-
-	return $lddlite_submit_processor->has_errors();
-}
-
-
-function ldl_has_global_errors() {
-	global $lddlite_submit_processor;
-
-	return $lddlite_submit_processor->has_global_errors();
-}
-
-
-function ldl_get_global_errors() {
-	global $lddlite_submit_processor;
-
-	return $lddlite_submit_processor->get_global_errors();
-}
-
-
+/**
+ * This is our shortcode callback for the submit listing form. It handles the display and the processing of the form
+ * through delegation to the ldl_submit... functions found in this file.
+ *
+ * @since 0.5.5
+ */
 function ldl_shortcode__submit() {
-	global $lddlite_submit_processor;
+    global $lddlite_submit_processor;
 
-	// Set up shortcode
     ldl_enqueue();
-    //wp_enqueue_script( 'lddlite-submit' );
-	//wp_enqueue_script( 'google-maps', 'http://maps.googleapis.com/maps/api/js?sensor=false&libraries=places' );
 
-	// Set up the processor
-	$lddlite_submit_processor = new ldd_directory_lite_processor;
+    // Set up the processor
+    $lddlite_submit_processor = new ldd_directory_lite_processor;
 
-	if ( $lddlite_submit_processor->is_processing() && !$lddlite_submit_processor->has_errors() ) {
-		do_action( 'lddlite_submit_pre_process', $lddlite_submit_processor );
+    if ($lddlite_submit_processor->is_processing() && !$lddlite_submit_processor->has_errors()) {
+        do_action('lddlite_submit_pre_process', $lddlite_submit_processor);
 
-		// Pull this into the local scope
-		$data = $lddlite_submit_processor->get_data();
+        if (ldl_submit_generate_listing()) {
+            ldl_get_template_part('submit', 'success');
+            do_action('lddlite_submit_post_process' $lddlite_submit_processor);
 
-/*		$user_id = is_user_logged_in() ? get_current_user_id() :ldl_submit__create_user( $data['username'], $data['email'] );
-		if ( !$user_id ) {
-			$lddlite_submit_processor->set_global_error( __( 'There was a problem creating your user account. Please try again later.', 'lddlite' ) );
-		}
+            return;
+        }
 
-		$post_id = ldl_submit__create_listing( $data['title'], $data['description'], $data['category'], $user_id );
-		if ( !$post_id ) {
-			$lddlite_submit_processor->set_global_error( __( 'There was a problem creating your listing. Please try again later.', 'lddlite' ) );
-		}*/
+    }
 
-		// Add all the post meta fields
-		//ldl_submit__create_meta( $data, $post_id );
-		ldl_submit__create_meta( $data, 0 );
-		mdd( $data );
-		// Upload their logo if one was submitted
-		if ( isset( $_FILES['ld_s_logo'] ) ) {
-			// These files need to be included as dependencies when on the front end.
-			require_once( ABSPATH . 'wp-admin/includes/image.php' );
-			require_once( ABSPATH . 'wp-admin/includes/file.php' );
-			require_once( ABSPATH . 'wp-admin/includes/media.php' );
-
-			$attachment_id = media_handle_upload( 'ld_s_logo', 0 );
-			set_post_thumbnail( $post_id, $attachment_id );
-		}
-
-		// Set these up so that they can be used on the success page as {{listing.field}}
-		foreach ( $data as $key => $value ) {
-			$data[ $key ] = htmlentities( $value );
-		}
-
-		ldl_submit__email_admin( $data, $post_id );
-		ldl_submit__email_owner( $data );
-
-		ldl_get_template_part( 'submit', 'success' );
-	} else {
-		ldl_get_template_part( 'submit' );
-	}
+    ldl_get_template_part('submit');
 
 }
 
-add_shortcode( 'directory_submit', 'ldl_shortcode__submit' );
+add_shortcode('directory_submit', 'ldl_shortcode__submit');
