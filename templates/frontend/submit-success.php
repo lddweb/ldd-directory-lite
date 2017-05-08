1986364804be854c1b6e67718315d80dc40fc5d9
LDDConsulting <web@lddconsulting.com>
2017-05-09 00:30:56 +0500

    Added support for default submission success and styling.

--- templates/frontend/submit-success.php
+++ templates/frontend/submit-success.php
@@ -1,6 +1,8 @@
 <div class="directory-lite">
     <?php ldl_get_header(); ?>
 
-    <?php echo ldl()->get_option('submit_success'); ?>
+    <div class="alert alert-success">
+        <?php echo ldl()->get_option('submit_success', '<h3>' . __('Congratulations!', 'ldd-directory-lite') . '</h3><p>' . __('Your listing has been successfully submitted for review. Please allow us sufficient time to review the listing and approve it for public display in our directory.', 'ldd-directory-lite') . '</p>'); ?>
+    </div>
 </div>
 
